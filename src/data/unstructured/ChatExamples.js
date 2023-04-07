2e1b8423cb259eb090b5c62f248672274e497e1d
LNshuti <nshutl0@sewanee.edu>
2023-04-07 11:29:37 -0700

    update local

---
+++ src/data/unstructured/ChatExamples.js
@@ -0,0 +1,24 @@
+const { ChatEngine } = require("prompt-engine");
+
+const description = "I want to speak with a bot which replies in under 20 words each time";
+const examples = [
+  { input: "Hi", response: "I'm a chatbot. I can chat with you about anything you'd like." },
+  { input: "Can you help me with the size of the universe?", response: "Sure. The universe is estimated to be around 93 billion light years in diameter." },
+];
+
+const flowResetText = "Forget the earlier conversation and start afresh";
+const promptEngine = new ChatEngine(description, examples, flowResetText, {
+  modelConfig: {
+    maxTokens: 1024,
+  }
+});
+promptEngine.addInteractions([
+  {
+    input: "What is the maximum speed an SUV from a performance brand can achieve?",
+    response: "Some performance SUVs can reach speeds over 150mph.",
+  },
+]);
+const prompt = promptEngine.buildPrompt("Can some cars reach higher speeds than that?");
+
+console.log("PROMPT\n\n" + prompt);
+console.log("PROMPT LENGTH: " + prompt.length);
